de3dd1424bc3d8a5c9d17be939ce481702dc4f1e
laidOffWorkerLearnsToCode <kwg6qlyv@hotmail.com>
2023-03-24 15:45:24 +0800

    Setup CSS file

--- RANDOM-USER-GEN/app.js
+++ RANDOM-USER-GEN/app.js
@@ -1,5 +1,13 @@
 const app = Vue.createApp({
-    template: '<h1>Hello World!</h1>'
+    data() {
+        return {
+            firstName: 'John',
+            lastName: 'Doe',
+            email: 'John@gmail.com',
+            gender: 'male',
+            picture:'https://randomuser.me/api/portraits/men/10.jpg'
+        }
+    }
 }); // 通过cdn引入vue.js后，可以直接使用Vue.createApp()方法创建一个Vue实例, 也可以使用Vue.component()方法创建一个组件, 一个Vue实例就是一个Vue应用，可以包含多个组件
 
 app.mount('#app'); // 通过Vue实例的mount()方法挂载到DOM元素上,这里是挂载到id为app的元素上
