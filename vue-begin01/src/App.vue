bed3c9260ee5bf47b775e42dd56aef61ce983d8b
laidOffWorkerLearnsToCode <kwg6qlyv@hotmail.com>
2023-04-13 16:06:35 +0800

    delete vue-begin01 project

---
+++ vue-begin01/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <button @click="count++">
+        Count is: {{ count }}
+    </button>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            count:0
+        }
+    }
+}
+
+</script>
+
+<style scoped>
+button{
+    font-weight: bold;
+}
+
+</style>
